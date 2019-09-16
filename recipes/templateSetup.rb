efb3ccc39879cca9e71ec05e8e3f875a24755368
ShuklaShubh89 <shubham.cored@gmail.com>
2019-09-16 18:00:49 +0000

    Version 0.2.1 : Added template for the motd file and created a templateSetup recipe

---
+++ recipes/templateSetup.rb
@@ -0,0 +1,25 @@
+package 'tree' do
+  action :install
+end 
+
+package 'ntp'
+
+package 'git' do
+  action :install
+end
+
+package 'nano'
+
+node['ipaddress']
+node['memory']['total']
+
+template '/etc/motd' do
+ source 'motd.erb' 
+ action :create
+ owner 'root'
+ group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end 
