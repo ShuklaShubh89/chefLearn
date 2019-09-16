08a4128953d992efc2443b6f399997994e0c4cf9
ShuklaShubh89 <shubham.cored@gmail.com>
2019-09-16 15:47:30 +0000

    Version 0.2.0 : Added node values through string interpolation

--- recipes/apacheServer.rb
+++ recipes/apacheServer.rb
@@ -2,8 +2,12 @@ package 'httpd' do
   action :install
 end 
 
+node['ipaddress']
+node['hostname']
+
 file '/var/www/html/index.html' do
-  content '<h1> Hello World! </h1>'
+  content "<h1>IP Address: #{node['ipaddress']} </h1> 
+<h1>Hostname: #{node['hostname']}  </h1>"
 end
 
 service 'httpd' do
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,8 +10,15 @@ end
 
 package 'nano'
 
+node['ipaddress']
+node['memory']['total']
+
 file '/etc/motd' do
- content 'This server is the property of Shubham'
+ content "This server is the property of Chef
+ HOSTNAME: #{node['ipaddress']}  
+ IPADDRESS: #{node['hostname']} 
+ CPU: #{node['cpu']['0']['model_name']}
+ MEM: #{node['memory']['total']}" 
  action :create
  owner 'root'
  group 'root'
